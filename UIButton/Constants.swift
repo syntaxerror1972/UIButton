a4a39e681d40c7002ef2fb951aba58aaf081daef
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2016-12-23 00:23:58 +0530

    Buttons are used for handling user actions. It intercepts the touch events and sends message to the target object.

---
+++ UIButton/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  UIButton
+//
+//  Created by Shrawan Shinde on 16/12/16.
+//  Copyright © 2016 Shrawan Shinde. All rights reserved.
+//
+
+import Foundation
+
+let firstButton = "Button1"
+let secondButton = "Button2"
+let thirdButton = "Button3"
--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var myFirstButton = UIButton()
+    var roundedButton = UIButton()
+    
+     var fontButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+        
+        //simple UIButton in swift 3.0
+        myFirstButton.setTitle(firstButton, for: .normal)
+        myFirstButton.setTitleColor(UIColor.black, for: .normal)
+        myFirstButton.backgroundColor=UIColor.red
+        myFirstButton.frame = CGRect(x: 150, y: 200, width: 150, height: 50)
+        myFirstButton.addTarget(self, action: #selector(self.myFirstButtonTapped), for: .touchUpInside)
+        
+        //simple UIButton with font in swift 3.0
+        fontButton.setTitle(secondButton, for: .normal)
+        fontButton.setTitleColor(UIColor.white, for: .normal)
+        fontButton.backgroundColor=UIColor.blue
+        fontButton.titleLabel?.font=UIFont(name: "Pacifico", size: 25)
+        fontButton.frame = CGRect(x: 150, y: 300, width: 150, height: 50)
+        fontButton.addTarget(self, action: #selector(self.fontButtonTapped), for: .touchUpInside)
+        
+        
+        //simple UIButton  withj rounded corner in swift 3.0
+        roundedButton.setTitle(thirdButton, for: .normal)
+        roundedButton.backgroundColor = .clear
+        roundedButton.layer.cornerRadius = 5
+        roundedButton.layer.borderWidth = 1
+        roundedButton.layer.borderColor = UIColor.black.cgColor
+        roundedButton.setTitleColor(UIColor.black, for: .normal)
+        roundedButton.frame = CGRect(x: 150, y: 400, width: 150, height: 50)
+        roundedButton.addTarget(self, action: #selector(self.roundedButtonTapped), for: .touchUpInside)
+        
+        self.view.addSubview(myFirstButton)
+        self.view.addSubview(fontButton)
+        self.view.addSubview(roundedButton)
     }
-
+    
+    func myFirstButtonTapped(sender : UIButton) {
+        //Write button action here
+        var alertView = UIAlertView();
+        alertView.addButton(withTitle: "Ok");
+        alertView.title = "Alert";
+        alertView.message = "myFirstButton Button Clicked";
+        alertView.show();
+    }
+    
+    func roundedButtonTapped(sender : UIButton) {
+        //Write button action here
+        var alertView = UIAlertView();
+        alertView.addButton(withTitle: "Ok");
+        alertView.title = "Alert";
+        alertView.message = "roundedButton Button Clicked";
+        alertView.show();
+    }
+    
+    func fontButtonTapped(sender : UIButton) {
+        //Write button action here
+        var alertView = UIAlertView();
+        alertView.addButton(withTitle: "Ok");
+        alertView.title = "Alert";
+        alertView.message = "fontButtonTapped Button Clicked";
+        alertView.show();
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
